7d69113f43302cc204ce32e8edc57e3535e5b8db
Gustavo Castro <gustavocastrocs@gmail.com>
2025-08-13 19:40:34 -0300

    aula 09

--- maratona-java/src/main/java/org/example/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ maratona-java/src/main/java/org/example/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -3,5 +3,17 @@ package org.example.academy.devdojo.maratonajava.introducao;
 public class OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("teste");
+
+        //comentário em linha
+
+        /*
+        comentário de multiplas plinhas
+         */
+
+        /**
+         * Isso é um comentário  javadov
+         */
+
+
     }
 }
