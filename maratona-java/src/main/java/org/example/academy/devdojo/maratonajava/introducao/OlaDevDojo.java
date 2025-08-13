f15e3115aaf5e20d676d7280a74d41bf90c52aab
Gustavo Castro <gustavocastrocs@gmail.com>
2025-08-13 19:09:18 -0300

    aula 08

---
+++ maratona-java/src/main/java/org/example/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,7 @@
+package org.example.academy.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+}
